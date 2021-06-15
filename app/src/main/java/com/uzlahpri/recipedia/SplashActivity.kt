07f710f67d1f692c3acded8bdecb6d6efbd712c5
priyanka uzlah <priyankauzlah@gmail.com>
2021-06-15 20:06:56 +0700

    fixing splash screen

--- app/src/main/java/com/uzlahpri/recipedia/SplashActivity.kt
+++ app/src/main/java/com/uzlahpri/recipedia/SplashActivity.kt
@@ -1,39 +1,26 @@
 package com.uzlahpri.recipedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.animation.AnimationUtils
 import com.uzlahpri.recipedia.databinding.ActivitySplashBinding
 import com.uzlahpri.recipedia.ui.MainActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private val SPLASH_TIME_OUT:Long = 6000
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
 
-    }
-
-    override fun onResume() {
-        val animCloud = AnimationUtils.loadAnimation(this, R.anim.splash_code)
-        binding.ivSplashLogo.animation = animCloud
-
-        object : CountDownTimer(3600, 200){
-            override fun onTick(p0: Long) {
-
-            }
-
-            override fun onFinish() {
-                startActivity(MainActivity.getLaunchService(this@SplashActivity))
-            }
-
-        }.start()
-
-        super.onResume()
+        Handler().postDelayed({
+            startActivity(Intent(this, SplashScreen::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
